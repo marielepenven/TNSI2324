81d15576d5635a64a1c31b821ac9c8a334e2dae4
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2023-10-23 17:25:00 +0200

    Update graph_succ_eleve.py

--- Graphes/Codes/graph_succ_eleve.py
+++ Graphes/Codes/graph_succ_eleve.py
@@ -4,19 +4,16 @@ class Graphe():
     
     def __init__(self,n):
         '''crée un graphe de taille n, en  n'indiquant par défaut, aucun arc'''
-        self.n=n
-        self.adj={}
-        for i in range(n):
-            self.adj[i]=[]
+        pass
         
     def ajouter_arc(self,s1,s2):
         '''ajoute un arc allant de s1 à s2 dans la matrice d'adjacence'''
-        self.adj[s1].append(s2)
+        pass
     
     def arc(self,s1,s2):
         '''renvoie True pour dire s'il existe un arc allant de s1 à s2 dans le graphe, False sinon'''
-        return s2 in self.adj[s1]
+        pass
         
     def voisins(self,s):
         '''renvoie la liste des voisins du sommet s'''
-        return self.adj[s]
+        pass 
