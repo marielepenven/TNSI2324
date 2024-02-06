62f69c219af2d0f2b37cd1079db12e64ebc6dcd4
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2024-02-06 12:00:12 +0100

    Update 22-NSI-14.py

--- EpreuvesPratiques/Sujet_14_2022/22-NSI-14.py
+++ EpreuvesPratiques/Sujet_14_2022/22-NSI-14.py
@@ -1,14 +1,3 @@
-def correspond(mot,mot_a_trous):
-    if len(mot)!=len(mot_a_trous):
-        return False
-    else:
-        for i in range(len(mot)):
-            if mot[i]!= mot_a_trous[i] and mot_a_trous[i] != "*":
-                return False
-        return True
-    
-            
-
 def est_cyclique(plan):
     '''
     Prend en paramètre un dictionnaire `plan` correspondant 
@@ -18,10 +7,10 @@ def est_cyclique(plan):
     et False sinon. 
     '''
     personne = 'A'
-    N = len(plan)                          
-    for i in range(N-1):
-        if plan[personne] == 'A':
+    N = len(....)                          
+    for i in range(....):
+        if plan[personne] == ....:
             return False
         else:
-            personne = plan[personne]
+            personne = .....
     return True
