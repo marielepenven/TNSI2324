fdd0562cd832f6d14597f49d9bcba5443aae92ef
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2024-02-09 10:26:11 +0100

    Add files via upload

---
+++ EpreuvesPratiques/sujet_03_2023/23-NSI-3-corrige.py
@@ -0,0 +1,56 @@
+def moyenne(element):
+    somme = 0
+    coeff = 0
+    for elt in element:
+        somme += elt[0]*elt[1]
+        coeff += elt[1]
+    if coeff == 0:
+        return None
+    else:
+        return somme/coeff
+    
+
+coeur = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
+         [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0], \
+         [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0], \
+         [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0], \
+         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], \
+         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], \
+         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], \
+         [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0], \
+         [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0], \
+         [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0], \
+         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], \
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+def affiche(dessin):
+    ''' affichage d'une grille : les 1 sont repreente par 
+        des "*" , les 0 par deux espaces "  " '''
+    for ligne in dessin:
+        for col in ligne:
+            if col == 1:
+                print(" *",end="")
+            else:
+                print("  ",end="")
+        print()
+
+
+def zoomListe(liste_depart,k):
+    '''renvoie une liste contenant k fois chaque 
+       element de liste_depart'''
+    liste_zoom = []
+    for elt in liste_depart :
+        for i in range(k):
+            liste_zoom.append(elt)
+    return liste_zoom
+
+def zoomDessin(grille,k):
+    '''renvoie une grille ou les lignes sont zoomees k fois 
+       ET repetees k fois'''
+    grille_zoom=[]
+    for elt in grille:
+        liste_zoom = zoomListe(elt,k)
+        for i in range(k):
+            grille_zoom.append(liste_zoom)
+    return grille_zoom
+
