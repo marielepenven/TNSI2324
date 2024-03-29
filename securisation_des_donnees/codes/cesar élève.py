18cb3958a220e1f72836d75d3a1a1c75ffcfd6aa
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2024-03-29 08:28:46 +0100

    Add files via upload

---
+++ securisation_des_donnees/codes/cesar élève.py
@@ -0,0 +1,51 @@
+Alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Frequence = {"E":0.121,"A":0.0711,"I":0.0659,"S":0.0651,"N":0.0639,"R":0.0607,
+             "T":0.0592,"O":0.0502,"L":0.0496,"U":0.0449,"D":0.0367,"C":0.318,"M":0.0262,"P":0.0249,"G":0.0123,
+             "B":0.014,"V":0.0111,"H":0.0111,"F":0.0111,"Q":0.0065,"Y":0.0046,"X":0.0038,"J":0.0034,
+             "K":0.0029,"W":0.0017,"Z":0.0015}
+
+
+def chiffrer_ligne(message ,cle):
+    ''' message: str, pas de caractère accentué
+        clé: int
+        return str, en majuscules
+        prend en paramétre une chaine de caractères, message à chiffrer et une clé, et renvoie le message chiffré
+        '''
+    
+   pass
+
+def dechiffrer_ligne(message,cle):
+     ''' message: str, pas de caractère accentué
+        clé: int
+        return str, en majuscules
+        prend en paramétre une chaine de caractères, message à déchiffrer et une clé, et renvoie le message en clair
+        '''
+    pass
+
+
+
+def chiffrer(document,cle):
+    '''
+    message = str , nom du fichier à chiffrer, attention aucun caractère accentué dans le document
+    clé = int
+    crée un document avec le message chiffré
+    '''
+    
+    pass 
+
+def dechiffrer(document,cle):
+     '''
+    message = str , nom du fichier chiffré, attention aucun caractère accentué dans le document
+    clé = int
+    crée un document avec le message en clair
+    '''
+   pass
+    
+def trouver_la_cle(document):
+     '''
+    message = str , nom du fichier à chiffré
+    trouve la clé du message chiffré
+    '''
+    pass 
+        
+    
